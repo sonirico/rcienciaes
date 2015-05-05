5da37b7a342673422f1bea1bcba9951237468920
sonirico <marsanben92@hotmail.com>
2015-05-05 15:26:04 +0200

    Add new task
    A new function called check_streaming has been added to playlist.tasks module in order to be able to resume the streaming after system reboots and failures.

--- playlist/tasks.py
+++ playlist/tasks.py
@@ -4,7 +4,8 @@
 from djcelery import celery
 from podget.views import DownloadManager
 from playlist.views import update_playlist
-
+from player.views import podcaster_in_the_air
+from mpc.models import MPDC, MPDCConnectionError
 
 dm = DownloadManager()
 # Both are invoked while the managers is not working
@@ -23,3 +24,13 @@ def update():
     if not dm.is_busy():
         update_playlist(None)
 
+
+@celery.task
+def check_streaming():
+    global dm
+    if not podcaster_in_the_air() and not dm.is_busy():
+        c = MPDC()
+        if c.connect():
+            if c.client.status().get('state') != 'play':
+                c.client.play()
+            c.client.close()
