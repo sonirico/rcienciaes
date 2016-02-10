1c088bac262ac3e93e5152ede019f7b400e0a469
sonirico <marsanben92@hotmail.com>
2016-02-10 20:32:01 +0100

    Cleaning

--- podcastmanager/urls.py
+++ podcastmanager/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.admin.views.decorators import staff_member_required
 
 urlpatterns = [
     url(r'^api/', include('api.urls')),
